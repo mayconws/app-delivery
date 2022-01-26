95cdd1535999eb7121fd91319dd8145c312053f0
Maycon Willian <mayconwas2018@gmail.com>
2022-01-26 15:10:05 -0300

    Criação do projeto app-delivery

---
+++ src/app/meu-primeiro/meu-primeiro-component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <h1>Testando primeiro Componente</h1>
+    `
+})
+export class MeuPrimeiroComponent{ }
